64a5c151f44e4b71a962345a8176f372132889a3
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2018-03-13 20:59:11 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ app.use(bodyParser.json());
 
 // ================================================================================
 // ROUTER
-// The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
